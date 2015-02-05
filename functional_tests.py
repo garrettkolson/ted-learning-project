a64c3d9c8b31b3aafce1f12861a77f6fb091f786
Garrett Olson <garrettolson@Garretts-MacBook-Pro.local>
2015-02-04 17:58:01 -0700

    Functional tests now checks we can input a to-do item.

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,22 +17,33 @@ class NewVisitorTest(unittest.TestCase):
 
 		# He notices the page title and header mention to-do lists
 		self.assertIn('', self.browser.title)
-		self.fail('Finish the test!')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('', header_text)
 
 		# He is invited to enter a to-do item straight away
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item:'
+		)
+
+		# She types "Code like crazy" into a text box (Garrett's hobby is Python coding)
+		inputbox.send_keys('Code like crazy')
+
+		# When he hits enter, the page updates, and now the page lists "1: Code like crazy"
+		# as an item in a to-do list
+		inputbox.send_keys(Keys.ENTER)
 		
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Code like crazy' for row in rows)
+		)
+		# There is still a text box inviting him to add another item. He enters
+		# "Code more and more and more!" (Garrett is very persistant)
+		self.fail('Finish the test!')
 
-# She types "Code like crazy" into a text box (Garrett's hobby is Python coding)
-
-# When he hits enter, the page updates, and now the page lists "1: Code like crazy"
-# as an item in a to-do list
-
-# There is still a text box inviting him to add another item. He enters
-# "Code more and more and more!" (Garrett is very persistant)
-
-# The page updates again, and now shows both items on his list
+		# The page updates again, and now shows both items on his list
 
 # Garrett wonders whether the site will remember his list. Then he sees that the site
 # has generated a unique URL for him -- there is some explanatory text to that effect.
@@ -39,5 +51,8 @@ if __name__ == '__main__':
 # He visits that URL - his to-do list is still there.
 
 # Satisfied, he goes back to sleep
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
 
 browser.quit()
